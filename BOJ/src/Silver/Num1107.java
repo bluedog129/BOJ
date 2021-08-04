188f620d8cfa476164638d27d9c069bbb6e4df6d
CHOI HYOJONG <50707297+bluedog129@users.noreply.github.com>
2021-08-04 10:36:35 +0900

    Num1107

---
+++ BOJ/src/Silver/Num1107.java
@@ -0,0 +1,13 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class Num1107 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+	}
+
+}
